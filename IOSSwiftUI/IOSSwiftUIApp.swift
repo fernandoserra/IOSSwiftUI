19be08d1cf238daebd7effa0b92cd2f41e29f08e
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-10 20:33:39 -0400

    Initial Commit

---
+++ IOSSwiftUI/IOSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSSwiftUIApp.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 10-04-22.
+//
+
+import SwiftUI
+
+@main
+struct IOSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
