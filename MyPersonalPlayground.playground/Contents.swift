9392ae102fd5fd19f77eee393a1741e234d731be
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-18 19:11:50 -0400

    Update PlayGround

--- MyPersonalPlayground.playground/Contents.swift
+++ MyPersonalPlayground.playground/Contents.swift
@@ -14,3 +14,20 @@ var result = Float(num) + num2
 
 print(result)
 
+
+print("Fernando","Serra", separator: " -*- ")
+
+
+func operacion(x:Int,y:Int) -> Int {
+    x+y
+}
+
+var resp = operacion(x: 2, y: 3)
+
+print(resp)
+
+func msj(param:String){
+    print("este es el mensaje: \(param) ")
+}
+
+msj(param: "de la funcion")
