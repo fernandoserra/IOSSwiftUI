410fa3d54daaad7a61f869b3c0f03bad93e666b6
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-18 18:45:28 -0400

    Add PlayGround

---
+++ MyPersonalPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+print(greeting)
+
+var num:Int = 14
+print(num)
+var num2:Float = 14.5
+print(num2)
+
+
+var result = Float(num) + num2
+
+print(result)
+
