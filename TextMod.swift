3ad405848cc9924156d5bec98a906fcf6574ac01
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-19 23:32:54 -0400

    Add TextMod

---
+++ TextMod.swift
@@ -0,0 +1,20 @@
+//
+//  TextMod.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 19-04-22.
+//
+
+import SwiftUI
+
+struct TextMod: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextMod_Previews: PreviewProvider {
+    static var previews: some View {
+        TextMod()
+    }
+}
