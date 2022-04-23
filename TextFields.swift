962509d196fe53227a0cd7accb8caf888861e5f8
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-23 12:58:09 -0400

    Update TextFields

--- TextFields.swift
+++ TextFields.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct TextFields: View {
-   @State var textView:String = "Fer"
+   @State var textView:String = ""
     
     
     var body: some View {
         
         VStack {
             Text(textView).bold()
+            
+            TextField("Ingresa la contraseña", text: $textView)
+                .padding(.horizontal, 30.0)
+            
             Button {
                 textView = "Fernando"
             } label: {
                 Text("Cambiando  el texto")
             }
             
-            
-
         }
     }
     
