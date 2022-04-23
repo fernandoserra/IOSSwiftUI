2f5f8f2af81e4b40be5e2c0f19b8c489a897c287
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-23 14:15:41 -0400

    Add Tab

---
+++ Tabwiews.swift
@@ -0,0 +1,20 @@
+//
+//  Tabwiews.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 23-04-22.
+//
+
+import SwiftUI
+
+struct Tabwiews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tabwiews_Previews: PreviewProvider {
+    static var previews: some View {
+        Tabwiews()
+    }
+}
