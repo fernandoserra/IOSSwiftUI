72525eb7c271e56b7247eab9828bb6cf6ba8264a
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-23 12:13:55 -0400

    Add file TextFields

---
+++ TextFields.swift
@@ -0,0 +1,20 @@
+//
+//  TextFields.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 23-04-22.
+//
+
+import SwiftUI
+
+struct TextFields: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFields_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFields()
+    }
+}
