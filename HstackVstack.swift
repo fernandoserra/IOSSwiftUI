c9069836beda1c1a1c01ac3d21b2d6ea72a5da4a
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-23 13:14:39 -0400

    Add HstackVstack

---
+++ HstackVstack.swift
@@ -0,0 +1,20 @@
+//
+//  HstackVstack.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 23-04-22.
+//
+
+import SwiftUI
+
+struct HstackVstack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HstackVstack_Previews: PreviewProvider {
+    static var previews: some View {
+        HstackVstack()
+    }
+}
