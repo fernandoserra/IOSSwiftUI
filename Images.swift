38bd549016b0fcb9047cc5e45bfc57c4e71c1c6d
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-23 11:42:09 -0400

    Add Images

---
+++ Images.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 23-04-22.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Images_Previews: PreviewProvider {
+    static var previews: some View {
+        Images()
+    }
+}
