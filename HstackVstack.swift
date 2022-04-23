c2ccf76137c8f8577f2cd004a691bb46ca8a2e04
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-23 13:33:41 -0400

    HstackVstack

--- HstackVstack.swift
+++ HstackVstack.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct HstackVstack: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //Alineando a la izquierda con .leading
+        //Alineando a la derecha con .trailing
+        VStack (alignment:.trailing) {
+           
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).border(Color.blue)
+            Spacer()
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            Spacer()
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            
+            HStack{
+                Text("HStack").frame(width: 100, height: 100, alignment: .center)
+                Spacer()
+                Text("HStack")
+                Text("HStack")
+                
+            }
+        }.border(Color.brown)
     }
 }
 
