4ffee2ecd9b4a7ca80c87cecc6bd301c45766f8f
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-24 11:43:33 -0400

    Add VPlayer

---
+++ VPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  VPlayer.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 24-04-22.
+//
+
+import SwiftUI
+
+struct VPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        VPlayer()
+    }
+}
