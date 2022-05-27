c9258ac166e9c08c1571d6438824e11f949d8922
Fernando Serra <fernando.serra.r@gmail.com>
2022-05-27 16:09:41 -0400

    Update SplashScreenView

--- SplashScreenView.swift
+++ SplashScreenView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive{
+            ContentView()
+        }else{
+            VStack{
+                VStack{
+                    Image(systemName: "person.crop.circle.fill").font(.system(size: 80))
+                    Text("Ejemplo Splash").font(Font.custom("Baskerville-Bold", size: 26))
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 1.2)){
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0 ){
+                    withAnimation{
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+     
     }
 }
 
