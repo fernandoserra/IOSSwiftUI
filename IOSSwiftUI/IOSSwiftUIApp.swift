7d527d1a665f52cc2b50fa71af851f3c8f93a9ee
Fernando Serra <fernando.serra.r@gmail.com>
2022-05-27 15:51:03 -0400

    Update IOSSwiftUIApp

--- IOSSwiftUI/IOSSwiftUIApp.swift
+++ IOSSwiftUI/IOSSwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct IOSSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            SplashScreenView()
         }
     }
 }
