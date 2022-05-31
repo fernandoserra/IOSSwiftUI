54be21c26fcb7f3ed045a36fff3d97db967969c2
Fernando Serra <fernando.serra.r@gmail.com>
2022-05-31 18:47:34 -0400

    Create ListPost

---
+++ view/ListPost.swift
@@ -0,0 +1,20 @@
+//
+//  ListPost.swift
+//  IOSSwiftUI
+//
+//  Created by Fernando Serra on 31-05-22.
+//
+
+import SwiftUI
+
+struct ListPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListPost_Previews: PreviewProvider {
+    static var previews: some View {
+        ListPost()
+    }
+}
