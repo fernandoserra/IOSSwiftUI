c0de8ac3d5bd568a6563735a0f9781a3b4b456d9
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-06 00:04:05 -0400

    Update ListPost

--- view/ListPost.swift
+++ view/ListPost.swift
@@ -13,6 +13,33 @@ struct ListPost: View {
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        ForEach(todosLosPost.postInfo, id:\.self){
+            post in
+    
+          
+            VStack(spacing:0){
+               /* VideoPlayer(player: AVPlayer(url: URL(string:juego.videosUrls.mobile)!))
+                    .frame(height: 300)
+                Text("\(juego.title)")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .frame(maxWidth:.infinity,alignment: .leading)
+                    .background(Color("Blue-Gray"))
+                    .clipShape(RoundedRectangle(cornerRadius: 3.0))*/
+                Text("\(post.title)")
+                    .foregroundColor(Color.red)
+                    .padding()
+                    .frame(maxWidth:.infinity,alignment: .leading)
+        
+                    .clipShape(RoundedRectangle(cornerRadius: 3.0))
+                
+            
+            }
+            
+           
+        }
     }
 }
 
